74dba26e968896821e3076b37bf2af8fcc79f3ad
FrancoisLB <francois.le.berre@free.fr>
2024-05-15 21:50:49 +0200

    correction du script [test ok]

--- install.sh
+++ install.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
-echo "Install startup_play and shutdown_play services." 
+echo "Installation des services startup_play et shutdown_play."
 
 # Utiliser grep pour filtrer les lignes contenant "Model"
 model=$(grep Model /proc/cpuinfo)
 
-# retourne par exemple :
-# Model           : Raspberry Pi 4 Model B Rev 1.1
- 
 # Extraire la version à partir de la ligne de révision
-version_pi=${model: -4}
+version_pi=$(echo "$model" | awk -F': ' '{print $2}')
 
 # Afficher la version du Raspberry Pi
 echo "Version du Raspberry Pi : $version_pi"
 
-echo "Install espeak"
-sudo apt install espeak
+echo "Installation de espeak"
+sudo apt install espeak -y
 
-echo "Create new directory : home/pi/audio to store audio files" 
-mkdir /home/pi/audio 
+echo "Création du répertoire /home/pi/audio pour stocker les fichiers audio" 
+mkdir -p /home/pi/audio 
 sudo chmod 777 /home/pi/audio 
 
-echo "Copy audio files in this directory" 
+echo "Copie des fichiers audio dans ce répertoire" 
 cp ./audio_fichier_waves/*.wav /home/pi/audio
 
-echo "sh files are copied in /etc directory."
-cp *.sh /etc
-chmod +x /etc/shutdownplay.sh
-chmod +x /etc/startupplay.sh
+echo "Copie des fichiers sh dans le répertoire /etc."
+sudo cp *.sh /etc
+sudo chmod +x /etc/shutdownplay.sh
+sudo chmod +x /etc/startupplay.sh
 
-echo "Copy of service files in /etc/systemd/system directory."
-cp *.service /etc/systemd/system 
+echo "Copie des fichiers de service dans le répertoire /etc/systemd/system."
+sudo cp *.service /etc/systemd/system 
 sudo chmod 644 /etc/systemd/system/startupplay.service
 sudo chmod 644 /etc/systemd/system/shutdownplay.service
 
-echo "Service activation" 
+echo "Activation des services" 
 sudo systemctl daemon-reload 
 sudo systemctl enable startupplay.service
 sudo systemctl enable shutdownplay.service
 
-echo "Restart services."
-sudo systemctl daemon-reload
+echo "Redémarrage des services."
 sudo systemctl restart startupplay.service
- 
+
+echo "Completed."
